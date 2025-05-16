423a5b0576d604fe8bb215ce45c20d25cdc95d3e
JinYang0318 <jyap384@gmail.com>
2025-05-16 21:00:18 +0800

    modify pom

--- src/main/java/com/orderflow_user_service/orderflow_user_svc/OrderflowUserSvcApplication.java
+++ src/main/java/com/orderflow_user_service/orderflow_user_svc/OrderflowUserSvcApplication.java
@@ -2,8 +2,10 @@ package com.orderflow_user_service.orderflow_user_svc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrderflowUserSvcApplication {
 
 	public static void main(String[] args) {
