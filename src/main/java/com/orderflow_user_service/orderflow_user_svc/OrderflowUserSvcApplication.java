2166f086ec3c20ce4e5123670419f3f4cad78e3d
JinYang0318 <jyap384@gmail.com>
2025-05-16 23:03:11 +0800

    test controller

--- src/main/java/com/orderflow_user_service/orderflow_user_svc/OrderflowUserSvcApplication.java
+++ src/main/java/com/orderflow_user_service/orderflow_user_svc/OrderflowUserSvcApplication.java
@@ -2,13 +2,13 @@ package com.orderflow_user_service.orderflow_user_svc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableDiscoveryClient
 public class OrderflowUserSvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderflowUserSvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderflowUserSvcApplication.class, args);
+    }
 }
